03c3c3ef64131fc40e6f42fe8a2c24821e146af4
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha25.aulas.gsyc.urjc.es>
2018-02-13 10:48:48 +0100

    Número aleatorio generado

--- servidor-random-http.py
+++ servidor-random-http.py
@@ -8,6 +8,7 @@ SAT subject (Universidad Rey Juan Carlos)
 """
 
 import socket
+import random
 
 # Create a TCP objet socket and bind it to a port
 # Port should be 80, but since it needs root privileges,
@@ -17,14 +18,22 @@ mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Let the port be reused if no process is actually using it
 mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Bind to the address corresponding to the main name of the host
-mySocket.bind((socket.gethostname(), 1235))
+mySocket.bind(('localhost', 1234))
 
 # Queue a maximum of 5 TCP connection requests
 mySocket.listen(5)
 
 # Accept connections, read incoming data, and answer back an HTML page
 #  (in an almost-infinite loop; the loop can be stopped with Ctrl+C)
-
+try:
+    while True:
+        print('Waiting for connections')
+        (recvSocket, address) = mySocket.accept()
+        print('Request received:')
+        print(recvSocket.recv(2048))
+        print('Answering back...')
+        aleat = str(random.randrange(999999999))
+        recvSocket.send(bytes(
 try:
     while True:
         print('Waiting for connections')
@@ -33,7 +42,7 @@ try:
         print(recvSocket.recv(2048))
         print('Answering back...')
         recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
-                        b"<html><body><h1>Hello World!</h1>" +
+                        b"<html><body><h1>Hello\r World!</h1>" +
                         b"<p>And in particular hello to you, " +
                         bytes(address[0], 'utf-8') +
                         b"</p>" +
