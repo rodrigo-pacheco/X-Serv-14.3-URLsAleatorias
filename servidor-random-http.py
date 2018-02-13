1d5e1193ef9a3a0792b09c061940ac4ad7bb1655
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha25.aulas.gsyc.urjc.es>
2018-02-13 10:30:10 +0100

    Restaruo versión de servidor-http-simple-2.py porque estaba teniendo problemas anteriormente

--- servidor-random-http.py
+++ servidor-random-http.py
@@ -4,33 +4,42 @@ Simple HTTP Server: serves random URLs usign a hyperlink
 
 Rodrigo Pacheco Martinez-Atienza
 r.pachecom @ gsyc.es
-SAT and SARO subjects (Universidad Rey Juan Carlos)
+SAT subject (Universidad Rey Juan Carlos)
 """
 
 import socket
 
 # Create a TCP objet socket and bind it to a port
-# We bind to 'localhost', therefore only accepts connections from the
-# same machine
 # Port should be 80, but since it needs root privileges,
 # let's use one above 1024
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySocket.bind(('localhost', 1234))
+# Let the port be reused if no process is actually using it
+mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# Bind to the address corresponding to the main name of the host
+mySocket.bind((socket.gethostname(), 1235))
 
 # Queue a maximum of 5 TCP connection requests
-
 mySocket.listen(5)
 
 # Accept connections, read incoming data, and answer back an HTML page
-#  (in an infinite loop) - Loop can only be stopped with ctrl+Z
+#  (in an almost-infinite loop; the loop can be stopped with Ctrl+C)
 
-while True:
-    print('Waiting for connections')
-    (recvSocket, address) = mySocket.accept()
-    print('HTTP request received:')
-    print(recvSocket.recv(1024))    # Buffer size 1024
-    recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
-                    b"<html><body><h1>Hello World!</h1></body></html>" +
-                    b"\r\n")
-    recvSocket.close()
+try:
+    while True:
+        print('Waiting for connections')
+        (recvSocket, address) = mySocket.accept()
+        print('Request received:')
+        print(recvSocket.recv(2048))
+        print('Answering back...')
+        recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
+                        b"<html><body><h1>Hello World!</h1>" +
+                        b"<p>And in particular hello to you, " +
+                        bytes(address[0], 'utf-8') +
+                        b"</p>" +
+                        b"</body></html>" +
+                        b"\r\n")
+        recvSocket.close()
+except KeyboardInterrupt:
+    print("Closing binded socket")
+mySocket.close()
